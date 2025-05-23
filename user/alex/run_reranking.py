69483299f7284c82f8fe4bf7c94b6a19bc15c4e0
palexbg <palexbg@users.noreply.github.com>
2025-05-23 16:32:38 +0200

    reranking done

---
+++ user/alex/run_reranking.py
@@ -0,0 +1,34 @@
+import reranker
+import yaml
+import glob
+import pandas as pd
+import re
+from box import ConfigBox
+import os 
+from pathlib import Path
+
+with open("user/alex/reranking-config.yml", "r") as file:
+    config_reranker = ConfigBox(yaml.safe_load(file))
+
+ROOTPATHPARQUET = 'data/'
+
+dates = set()
+for f in os.listdir(ROOTPATHPARQUET):
+    match = re.search(r"(date=\d{4}-\d{2})", f) #match the date format that they put up
+    if match:
+        dates.add(match.group(1)) 
+
+reranker_model = reranker.Rerank(config_reranker)
+    
+for date in list(dates):
+    df = pd.read_parquet(f'data/{date}/').sort_values(by=['qid', 'score'], ascending=[True, False]).reset_index().drop('index', axis=1).head(10000)
+   
+    result = reranker_model.rerank_stage(data=df)
+    # @Anthony, we can also merge them back together optionally, if needed
+    # result = pd.merge(df, result)
+    out_dir = Path(f"{config_reranker.data.output_folder}/{date}")
+    out_dir.mkdir(parents=True, exist_ok=True) 
+    result.to_csv(f'{out_dir}/reranked_results_{date}.csv')
+    
+
+
