283d8726a37bc821503513bd09cdfc217041bc1c
palexbg <56965360+palexbg@users.noreply.github.com>
2025-05-24 03:31:16 -0400

    parralelizing slurm job

--- user/alex/run_reranking.py
+++ user/alex/run_reranking.py
@@ -10,26 +10,31 @@ from pathlib import Path
 with open("user/alex/reranking-config.yml", "r") as file:
     config_reranker = ConfigBox(yaml.safe_load(file))
 
+import argparse
+
+# target date is passed in as an argument
+parser = argparse.ArgumentParser()
+parser.add_argument("--date", type=str, required=True)
+args = parser.parse_args()
+
+target_date = args.date
+
 ROOTPATHPARQUET = config_reranker.data.input_rootfolder
 
-dates = set()
-for f in os.listdir(ROOTPATHPARQUET):
-    match = re.search(r"(date=\d{4}-\d{2})", f) #match the date format that they put up
-    if match:
-        dates.add(match.group(1)) 
-
+# load reranker
 reranker_model = reranker.Rerank(config_reranker)
     
-for date in list(dates):
-    print(f'Running reranking for dataset: {date} now')
-    df = pd.read_parquet(f'{ROOTPATHPARQUET}/{date}/').sort_values(by=['qid', 'score'], ascending=[True, False]).reset_index().drop('index', axis=1)
+
+# run reranking
+print(f'Running reranking for dataset: {target_date} now')
+df = pd.read_parquet(f'{ROOTPATHPARQUET}/{target_date}/').sort_values(by=['qid', 'score'], ascending=[True, False]).reset_index().drop('index', axis=1)
    
-    result = reranker_model.rerank_stage(data=df)
-    # @Anthony, we can also merge them back together optionally, if needed
-    # result = pd.merge(df, result)
-    out_dir = Path(f"{config_reranker.data.output_folder}/{date}")
-    out_dir.mkdir(parents=True, exist_ok=True) 
-    result.to_csv(f'{out_dir}/reranked_results_{date}.csv')
+result = reranker_model.rerank_stage(data=df)
+# @Anthony, we can also merge them back together optionally, if needed
+# result = pd.merge(df, result)
+out_dir = Path(f"{config_reranker.data.output_folder}/{target_date}")
+out_dir.mkdir(parents=True, exist_ok=True) 
+result.to_csv(f'{out_dir}/reranked_results_{target_date}.csv')
     
 
 
